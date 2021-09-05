317d8dccc8dc2ef43922d65a2981d85f0c31c1b1
BlossomWhale <85476751+BlossomWhale@users.noreply.github.com>
2021-09-05 21:30:48 +0900

    Initial commit2

---
+++ src/main/java/com/springbootawspractice/springbootawspractice/SpringbootAwsPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.springbootawspractice.springbootawspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootawspractice/springbootawspractice/SpringbootAwsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootawspractice.springbootawspractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
