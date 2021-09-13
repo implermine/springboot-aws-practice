800e436fd12930672fe1990b9237e999a8ddd791
BlossomWhale <85476751+BlossomWhale@users.noreply.github.com>
2021-09-14 04:42:49 +0900

    none

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -3,7 +3,7 @@
 CURRENT_PORT=$(cat /home/ec2-user/service_url.inc | grep -Po '[0-9]+' | tail -1)
 TARGET_PORT=0
 
-echo "> Nginx currently prixies to ${CURRENT_PORT}."
+echo "> Nginx currently proxies to ${CURRENT_PORT}."
 
 # Toggle port number
 if [ ${CURRENT_PORT} -eq 8081 ]; then
@@ -18,9 +18,9 @@ fi
 # Change proxying port into target port
 echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" | tee /home/ec2-user/service_url.inc
 
-echo ">Now Nginx proxies to ${TARGET_PORT}."
+echo "> Now Nginx proxies to ${TARGET_PORT}."
 
 # Reload nginx
-sudo service nginx Reload
+sudo service nginx reload
 
 echo "> Nginx reloaded."
